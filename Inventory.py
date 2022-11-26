cb919c3cb8b2431bb1298a853bfc89704cc06cd7
Jason Weeks <chandlerjweeks0@gmail.com>
2022-11-25 18:48:26 -0600

    combined methods into a single class
    Took getters and add item functions created in individual files and merged them into a single inventory class

---
+++ Inventory.py
@@ -0,0 +1,48 @@
+import mysql.connector
+import sys
+
+try:
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="project1"
+    )
+
+    print("Successful connection.")
+
+except:
+    print("Failed connection.")
+    sys.exit()
+
+cursor = connection.cursor()
+
+
+# Getters all find matching items from table
+def get_item_ID(item_ID):
+    return cursor.execute("SELECT * FROM 'Inventory' WHERE itemID = '{}'".format(item_ID))
+
+
+def get_name(name):
+    return cursor.execute("SELECT * FROM 'Inventory' WHERE Name = '{}'".format(name))
+
+
+
+
+class Inventory:
+    # Creation of a new item in inventory
+    def __init__(self, item_ID, name, price, rating, stock):
+        self.item_ID = item_ID
+        self.name = name
+        self.price = price
+        self.rating = rating
+        self.stock = stock
+
+        cursor.execute("INSERT INTO `inventory`(`ItemID`, `ItemName`, `Price`, `Rating`, `Stock`) "
+                       "VALUES ('{}','{}','{}','{}','{}')".format(item_ID, name, price, rating, stock))
+        connection.commit()
+
+
+
+
+
