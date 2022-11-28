96f6663b98398a4c18f5dcad03fc09cd71f27da2
Bryanj101 <112009864+Bryanj101@users.noreply.github.com>
2022-11-28 16:35:52 -0600

    Update book-example.py

--- book-example.py
+++ book-example.py
@@ -21,13 +21,28 @@ cursor = connection.cursor()
 class Book:
     def __init__(self):
         self.book_list = None
+        self.book_price = None
+        self.book_stock = None
 
     def display_books(self):
         cursor.execute("SELECT * FROM book")
         self.book_list = cursor.fetchall()
 
         for x in range(len(self.book_list)):
-            print(f'Title: {self.book_list[x][1]}', f'ISBN: {self.book_list[x][2]}', f'Author: {self.book_list[x][3]}')
+            cursor.execute(f"SELECT Price FROM inventory WHERE ItemName = '{self.book_list[x][1]}'")
+            self.book_price = cursor.fetchall()
+
+            cursor.execute(f"SELECT Stock FROM inventory WHERE ItemName = '{self.book_list[x][1]}'")
+            self.book_stock = cursor.fetchall()
+
+            if self.book_stock[0][0] == 0 or self.book_stock[0][0] is None:
+                print(f'Title: {self.book_list[x][1]}', f'ISBN: {self.book_list[x][2]}',
+                      f'Author: {self.book_list[x][3]}',
+                      f'Price: ${self.book_price[0][0]}', f'Out of Stock!')
+            else:
+                print(f'Title: {self.book_list[x][1]}', f'ISBN: {self.book_list[x][2]}',
+                      f'Author: {self.book_list[x][3]}',
+                      f'Price: ${self.book_price[0][0]}', f'Stock: {self.book_stock[0][0]}')
 
 
 Book().display_books()
