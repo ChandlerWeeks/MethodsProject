dc4b8058a5ebaa43fdda0c952d6d4664e27922b1
William-Emmich <117790219+William-Emmich@users.noreply.github.com>
2022-11-28 16:45:06 -0600

    Update order.py

--- order.py
+++ order.py
@@ -11,43 +11,54 @@ try:
         database="project1"
     )
 
+    print("Successful connection.")
+
 except:
     print("Failed connection.")
     sys.exit()
 
 cursor = connection.cursor()
 
+
 class Order:
-    #constructor
-    def __init__(self,cartID, orderID, orderTime, userID, itemID, quantity,date,time):
-        self.cartID = cartID
-        self.orderID = orderID
-        self.orderTime = orderTime
+    # constructor
+    def __init__(self, userID):
+        self.cartID = None
+        self.orderID = None
+        self.orderTime = None
         self.userID = userID
-        self.itemID = itemID
-        self.quantity = quantity
-        #didnt use these for date and time?
-        self.date = date
-        self.time = time
-    #getters per class diagram
-    def get_CartID(cartID):
-        cursor.execute("SELECT * FROM 'orders' WHERE cartID = '{}'".format(cartID))
+        self.itemID = None
+        self.quantity = None
+        # didnt use these for date and time?
+        self.date = None
+        self.time = None
+        self.ordereditems = {}
+
+    # getters per class diagram
+    def get_CartID(self):
+        cursor.execute("SELECT CartID FROM orders WHERE UserID = '{}'".format(self.userID))
         return cursor.fetchall()
-    def get_OrderID(orderID):
-        cursor.execute("SELECT * FROM 'orders' WHERE orderID = '{}'".format(orderID))
+
+    def get_OrderID(self):
+        cursor.execute("SELECT OrderID FROM orders WHERE UserID = '{}'".format(self.userID))
         return cursor.fetchall()
-    def get_OrderTime(orderTime):
-        cursor.execute("SELECT * FROM 'orders' WHERE orderTime = '{}'".format(orderTime))
+
+    def get_OrderTime(self):
+        cursor.execute("SELECT DateTime FROM orders WHERE UserID = '{}'".format(self.userID))
         return cursor.fetchall()
-    def get_UserID(userID):
-        cursor.execute("SELECT * FROM 'orders' WHERE userID = '{}'".format(userID))
+
+    def get_UserID(self):
+        cursor.execute("SELECT UserID FROM orders WHERE UserID = '{}'".format(self.userID))
         return cursor.fetchall()
-    def get_ItemID(itemID):
-        cursor.execute("SELECT * FROM 'orders' WHERE itemID = '{}'".format(itemID))
+
+    def get_ItemID(self):
+        cursor.execute("SELECT ItemID FROM orderitems WHERE UserID = '{}'".format(self.userID))
         return cursor.fetchall()
-    def get_Quanity(quantity):
-        cursor.execute("SELECT * FROM 'orders' WHERE quantity = '{}'".format(quantity))
+
+    def get_Quanity(self):
+        cursor.execute("SELECT Quantity FROM orderitems WHERE ItemID = '{}'".format(self.itemID))
         return cursor.fetchall()
+
     """def set_date(self, date):
         date1 = date.today()
         self.date = date1
@@ -62,25 +73,49 @@ class Order:
         self.time= ct
     def get_time(self):
         return self.time"""
+
     # sets value to emty
-    def deleteHistory(userID):
-        for key,value in ordereditems.items():
-            if key == userID:
+    def deleteHistory(self):
+        for key, value in self.ordereditems.items():
+            if key == self.userID:
                 value = ""
-    def orderHistory(userID):
-        global ordereditems
-        itemsOrdered = "SELECT ItemName FROM 'orderitems' WHERE userID = '{}'".format(userID)
+#add price and order id
+    def orderHistory(self):
+        itemsOrdered = "SELECT ItemName FROM orderitems WHERE userID = '{}'".format(self.userID)
         cursor.execute(itemsOrdered)
         items = cursor.fetchall()
-        #checking if user already made an order
-        for key in ordereditems:
-            if key == userID:
-                ordereditems[key].append(items)
-        #if not just create new key value
+
+        cursor.execute("SELECT DateTime FROM orderitems WHERE userID = '{}'".format(self.userID))
+        time = cursor.fetchall()
+        # format the time but since tuples immutable convert to list, change time, convert to tuple
+        list2 = list(time)
+        for x in range(len(list2)):
+            for i in list2[x]:
+                list2[x] = i.strftime("%m/%d/%Y %H:%M:%S")
+        time = tuple(list2)
+        # checking if user already made an order
+        for key in self.ordereditems:
+            if key == self.userID:
+                self.ordereditems[key].append(items, time)
+        # if not just create new key value
         else:
-            dict1 = {userID:[items, date.today(), datetime.now()]}
-            ordereditems.update(dict1)
-        #print values
-        for key, value in ordereditems.items():
-            if key == userID:
-                print(value)
+            dict1 = {self.userID: [items, time]}
+            self.ordereditems.update(dict1)
+        # print values
+        for key, value in self.ordereditems.items():
+            if key == self.userID:
+                for x in range(len(value[0])):
+                    for i in value[0][x]:
+                        print(i, value[1][x])
+
+
+
+# example method for datetime object printing
+# grabs datetime tuples at that element and converts them into a properly formatted string
+"""for x in range(len(value[1])):
+    for i in value[1][x]:
+        print(i.strftime("%m/%d/%Y %H:%M:%S"))"""
+
+userId = 1
+Order(userId).orderHistory()
+
