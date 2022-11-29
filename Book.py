a1123d1cd9d7a80f17cf29c7f0987f5d44c4172d
MelodyFlight <117954437+MelodyFlight@users.noreply.github.com>
2022-11-28 18:41:33 -0600

    Update Book.py

--- Book.py
+++ Book.py
@@ -23,5 +23,18 @@ class Book:
         cursor.execute("SELECT * FROM book ORDER BY Title")
         self.book_list = cursor.fetchall()
 
-        for x in self.book_list:
-            print(x)
+        for x in range(len(self.book_list)):
+            cursor.execute(f"SELECT Price FROM inventory WHERE ItemName = '{self.book_list[x][1]}'")
+            self.book_price = cursor.fetchall()
+
+            cursor.execute(f"SELECT Stock FROM inventory WHERE ItemName = '{self.book_list[x][1]}'")
+            self.book_stock = cursor.fetchall()
+
+            if self.book_stock[0][0] == 0 or self.book_stock[0][0] is None:
+                print(f'Title: {self.book_list[x][1]}', f'ISBN: {self.book_list[x][2]}',
+                      f'Author: {self.book_list[x][3]}',
+                      f'Price: ${self.book_price[0][0]}', f'Out of Stock!')
+            else:
+                print(f'Title: {self.book_list[x][1]}', f'ISBN: {self.book_list[x][2]}',
+                      f'Author: {self.book_list[x][3]}',
+                      f'Price: ${self.book_price[0][0]}', f'Stock: {self.book_stock[0][0]}')
