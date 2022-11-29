bf6b686354c5ea77d00b92142b6070d3c4f698f5
MelodyFlight <117954437+MelodyFlight@users.noreply.github.com>
2022-11-28 20:10:17 -0600

    Update Movie.py

--- Movie.py
+++ Movie.py
@@ -19,9 +19,16 @@ class Movie:
     def __init__(self):
         self.movie_list = None
 
-    def display_movies(self):
-        cursor.execute("SELECT * FROM movie ORDER BY Title")
-        self.movie_list = cursor.fetchall()
+    for x in range(len(self.movie_list)):
+            cursor.execute(f"SELECT Price FROM inventory WHERE ItemName = '{self.movie_list[x][1]}'")
+            self.movie_price = cursor.fetchall()
 
-        for x in self.movie_list:
-            print(x)
+            cursor.execute(f"SELECT Stock FROM inventory WHERE ItemName = '{self.movie_list[x][1]}'")
+            self.movie_stock = cursor.fetchall()
+
+            if self.movie_stock[0][0] == 0 or self.movie_stock[0][0] is None:
+                print(f'Title: {self.movie_list[x][1]}', f'Price: ${self.movie_price[0][0]}',
+                      f'Description: {self.movie_list[x][4]}', f'Out of Stock!')
+            else:
+                print(f'Title: {self.movie_list[x][1]}', f'Price: ${self.movie_price[0][0]}', 
+                      f'Description: {self.movie_list[x][4]}', f'Stock: {self.movie_stock[0][0]}')
