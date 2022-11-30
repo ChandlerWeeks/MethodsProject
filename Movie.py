dde13420865428d6f62a66da5c6d31eca3d63e3f
Bryanj101 <112009864+Bryanj101@users.noreply.github.com>
2022-11-30 15:50:30 -0600

    Update Movie.py

--- Movie.py
+++ Movie.py
@@ -18,17 +18,23 @@ cursor = connection.cursor()
 class Movie:
     def __init__(self):
         self.movie_list = None
+        self.movie_price = None
+        self.movie_stock = None
 
-    for x in range(len(self.movie_list)):
-            cursor.execute(f"SELECT Price FROM inventory WHERE ItemName = '{self.movie_list[x][1]}'")
-            self.movie_price = cursor.fetchall()
+    def display_movies(self):
+        cursor.execute("SELECT * FROM movie ORDER BY Title")
+        self.movie_list = cursor.fetchall()
 
-            cursor.execute(f"SELECT Stock FROM inventory WHERE ItemName = '{self.movie_list[x][1]}'")
-            self.movie_stock = cursor.fetchall()
+        for x in range(len(self.movie_list)):
+                cursor.execute(f"SELECT Price FROM inventory WHERE ItemName = '{self.movie_list[x][1]}'")
+                self.movie_price = cursor.fetchall()
 
-            if self.movie_stock[0][0] == 0 or self.movie_stock[0][0] is None:
-                print(f'Title: {self.movie_list[x][1]}', f'Price: ${self.movie_price[0][0]}',
-                      f'Description: {self.movie_list[x][4]}', f'Out of Stock!')
-            else:
-                print(f'Title: {self.movie_list[x][1]}', f'Price: ${self.movie_price[0][0]}', 
-                      f'Description: {self.movie_list[x][4]}', f'Stock: {self.movie_stock[0][0]}')
+                cursor.execute(f"SELECT Stock FROM inventory WHERE ItemName = '{self.movie_list[x][1]}'")
+                self.movie_stock = cursor.fetchall()
+
+                if self.movie_stock[0][0] == 0 or self.movie_stock[0][0] is None:
+                    print(f'Title: {self.movie_list[x][1]}', f'Price: ${self.movie_price[0][0]}',
+                          f'Description: {self.movie_list[x][4]}', f'Out of Stock!')
+                else:
+                    print(f'Title: {self.movie_list[x][1]}', f'Price: ${self.movie_price[0][0]}',
+                          f'Description: {self.movie_list[x][4]}', f'Stock: {self.movie_stock[0][0]}')
